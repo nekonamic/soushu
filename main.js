c81cbd3e5fd7760d5c5f22785bf0f840dc2f411b
nekonamic <nekonamic@hotmail.com>
2025-08-24 21:37:22 +0800

    reboot

--- main.js
+++ main.js
@@ -16,16 +16,19 @@ db.prepare(`
     )
 `).run();
 
-db.close();
-
 (async () => {
     const domain = 'ac1ss.ascwefkjw.com'
     const baseUrl = 'https://ac1ss.ascwefkjw.com/'
-    const mobileTXTPath = ''
+    const mobileTXTPath = 'forum.php?mod=forumdisplay&fid=72'
 
-    const browser = await chromium.launch({ headless: true });
+    const browser = await chromium.launch({
+        headless: true,
+        proxy: {
+            server: "http://127.0.0.1:7890"
+        }
+    });
     const context = await browser.newContext();
-    const cookieString = `PHPSESSID=fehap6pvv43kuu7r4foskqi2dp; yj0M_5a0c_ulastactivity=1755685095%7C0; yj0M_5a0c_saltkey=wTb5t5gB; yj0M_5a0c_lastvisit=1755680847; yj0M_5a0c_lastact=1755685522%09forum.php%09viewthread; yj0M_5a0c__refer=%252Fhome.php%253Fmod%253Dspacecp%2526ac%253Dprofile%2526op%253Dpassword; yj0M_5a0c_auth=cf10GXGgtSukhwCS4cImZ9JYBSVvTghCdTsq1ht4qyebLeDNdTprQaXQm7vXC%2FDyke9L51ISDV24Z%2FENcwBPiu43hek; yj0M_5a0c_lastcheckfeed=588604%7C1755684455; yj0M_5a0c_lip=47.79.94.249%2C1755684455; yj0M_5a0c_sid=0; yj0M_5a0c_nofavfid=1; yj0M_5a0c_st_t=588604%7C1755685250%7C1cb744b1014d3a5af96469584e0de3cb; yj0M_5a0c_forum_lastvisit=D_102_1755684843D_72_1755684870D_103_1755685011D_40_1755685250; yj0M_5a0c_smile=1D1; yj0M_5a0c_st_p=588604%7C1755685522%7C591978a0f584979afc565ead1ec9e755; yj0M_5a0c_viewid=tid_1324402`;
+    const cookieString = `PHPSESSID=fehap6pvv43kuu7r4foskqi2dp; yj0M_5a0c_saltkey=FEBk1Iww; yj0M_5a0c_lastvisit=1756033217; yj0M_5a0c_lastact=1756037036%09index.php%09; yj0M_5a0c_st_t=0%7C1756036817%7Cabc85afee8e05d695b2929267559e611; yj0M_5a0c_sendmail=1; yj0M_5a0c_ulastactivity=1756037036%7C0; yj0M_5a0c_auth=fd77dVk912FxISusbqKRBKVlMjs03ZZkRYjdjkPWvFCSiZRwhxuZbdKDf2tWbf%2FUKYjlllF4mmvMaW2ssR09ck9desRe; yj0M_5a0c_lastcheckfeed=2527028%7C1756037036; yj0M_5a0c_checkfollow=1; yj0M_5a0c_lip=47.79.94.249%2C1756037036; yj0M_5a0c_sid=0`;
 
     const cookies = cookieString.split('; ').map(item => {
         const [name, ...rest] = item.split('=');
@@ -40,7 +43,6 @@ db.close();
         };
     });
 
-    // 添加 Cookie
     await context.addCookies(cookies);
     const page = await context.newPage();
 
@@ -57,15 +59,13 @@ db.close();
                     const link = threadList.nth(i).locator('a.s.xst').first();
                     const href = await link.getAttribute('href');
 
-                    const parsedUrl = new URL(baseUrl + href);
-                    const tid = parsedUrl.searchParams.get('tid');
-
                     if (href.includes('tid=adver')) {
                         console.log('adver')
                         continue;
                     }
 
-                    const db = new Database('./novel.db');
+                    const parsedUrl = new URL(baseUrl + href);
+                    const tid = parsedUrl.searchParams.get('tid');
 
                     const row = db.prepare('SELECT tid FROM novels WHERE tid = ?').get(tid);
 
@@ -88,37 +88,47 @@ db.close();
 
                     let title = ''
 
-                    try {
-                        let titleElement = threadPage.locator('span#thread_subject').first();
-                        title = await titleElement.textContent();
-                        console.log(title)
-                    } catch (err) {
-                        console.warn("title-loop");
-                        i--;
-                        continue;
-                    }
+                    let titleElement = threadPage.locator('span#thread_subject').first();
+                    title = await titleElement.textContent();
+                    console.log(title)
 
-                    const aList = threadPage.locator('a');
+                    // try {
+                    //     let titleElement = threadPage.locator('span#thread_subject').first();
+                    //     title = await titleElement.textContent();
+                    //     console.log(title)
+                    // } catch (err) {
+                    //     console.warn("title-loop");
+                    //     i--;
+                    //     continue;
+                    // }
+
+                    const aList = threadPage.locator('div.pcb:first-of-type a');
                     const count = await aList.count();
+                    let content = ''
 
                     for (let i = 0; i < count; i++) {
                         const href = await aList.nth(i).getAttribute('href')
                         if (href != null) {
                             if (href.startsWith('forum.php?mod=attachment&aid=')) {
                                 downloadLink = baseUrl + href;
-                                break;
+                                const response = await threadPage.request.get(downloadLink, { setTimeout: 6000000 });
+                                const buffer = await response.body();
+
+                                const detected = jschardet.detect(buffer);
+                                let encoding = (detected.encoding || "utf-8").toLowerCase();
+
+                                if (encoding === "gb2312" || encoding === "gbk") {
+                                    encoding = "gb18030";
+                                }
+
+                                const content = iconv.decode(buffer, encoding);
+                                // if (detected.encoding == 'UTF-8') {
+                                //     content += '\n' + iconv.decode(buffer, 'UTF-8');
+                                // } else {
+                                //     content += '\n' + iconv.decode(buffer, 'GB18030');
+                                // }
                             }
                         }
-                    }
-                    const response = await threadPage.request.get(downloadLink);
-                    const buffer = await response.body();
-
-                    const detected = jschardet.detect(buffer);
-                    let content = ''
-                    if (detected.encoding == null) {
-                        content = iconv.decode(buffer, 'UTF-8');
-                    } else {
-                        content = iconv.decode(buffer, detected.encoding);
                     }
 
                     try {
@@ -126,8 +136,6 @@ db.close();
                         stmt.run(tid, title, content);
                     } catch (err) {
                         console.error(err);
-                    } finally {
-                        db.close();
                     }
 
                     await threadPage.close()
@@ -148,6 +156,7 @@ db.close();
         }
     }
 
+    db.close();
     await browser.close();
 })();
 
